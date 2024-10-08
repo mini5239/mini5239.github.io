1daa4a5998c457d835117060e9e1c18a35afb9a5
mini5239 <kajung4928@naver.com>
2024-10-08 13:44:07 +0900

    second state profile

---
+++ src/pages/HomeView.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const HomeView=()=>{
+    return(
+        <div>
+            <Header/>
+            <Main>
+                <Intro/>
+                <Ability/>
+                <Site/>
+                <Project/>
+                <Contact/>
+            </Main>
+            <Footer/>
+        </div>
+    )
+}
+
+export default HomeView;
